53e416a882299cb240a47d9e148f066501de025a
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-02-20 09:12:12 +0100

    feat: add the user model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""User model
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Attributes:
+        id, the integer primary key
+        email, a non-nullable string
+        hashed_password, a non-nullable string
+        session_id, a nullable string
+        reset_token, a nullable string
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
