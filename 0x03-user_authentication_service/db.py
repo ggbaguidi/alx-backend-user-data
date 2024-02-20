70d2a93e26502634e16a36344688e17c884b089b
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-02-20 10:03:13 +0100

    feat: add find user by arguments

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -43,3 +45,21 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Params:
+            arbitrary keyword arguments
+        Returns:
+            returns the first row found in the users
+        """
+        all_users = self._session.query(User)
+
+        for key, value in kwargs.items():
+            if key not in User.__dict__:
+                raise InvalidRequestError
+            for user in all_users:
+                if getattr(user, key) == value:
+                    return user
+
+        raise NoResultFound
