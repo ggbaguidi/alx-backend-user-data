696d3fd68eccd4bdc8be9961bfdf8f3dd49cc491
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-02-20 10:47:05 +0100

    feat: add register user method to the auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,39 @@
 """Auth Module
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """Hash password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Params:
+            email: str
+            password: str
+        Returns:
+            User object
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = self._db.add_user(
+                email=email,
+                hashed_password=_hash_password(password)
+            )
+            return user
+
+        raise ValueError(f"User {email} already exists")
