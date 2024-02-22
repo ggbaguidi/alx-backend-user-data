a027e235f08d62ee0066c53b704c8ffd39077ad5
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-02-22 13:48:16 +0100

    feat: log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """FLask App module
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -47,6 +47,20 @@ def login() -> str:
     response.set_cookie("session_id", session_id)
 
     return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """Log out
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id=session_id)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user_id=user.id)
+    redirect('/')
 
 
 if __name__ == "__main__":
