c3ee506fae2be50af8fcf02aef6c410a47288016
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-02-22 14:39:05 +0100

    fix: update password error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,6 +109,9 @@ class Auth:
             ) from e
         hashed_password = _hash_password(password=password).decode('utf-8')
 
-        self._db.update_user(user_id=user.id, hashed_password=hashed_password)
+        self._db.update_user(
+            user_id=user.id,
+            hashed_password=hashed_password,
+            reset_token=None)
 
         return None
