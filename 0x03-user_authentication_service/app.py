5e98e48b03baa2cc26bac1391b4f56eef9448405
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-02-22 14:16:47 +0100

    feat: get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -76,5 +76,18 @@ def profile() -> str:
     return jsonify({"email": f"{user.email}"})
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """Get reset password token
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email=email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": f"{email}", "reset_token": f"{reset_token}"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
